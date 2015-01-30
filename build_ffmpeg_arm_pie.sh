39407340e6a6b0528f604999f03dac692770ae73
Coltin Caverhill <Coltin@yelp.com>
2015-01-30 07:53:12 -0800

    Enables Position Independent Executable (PIE) for arm processors.
    We are not going to support x86 at this time, but we may in the future.

--- build_ffmpeg_arm_pie.sh
+++ build_ffmpeg_arm_pie.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export NUM_JOBS=8
-export LEVEL=9
+export LEVEL=16
 export CPU=arm
 export PREFIX=arm-linux-androideabi
 export TOOLCHAIN_PREFIX=${PREFIX}
-export LIBVPX_TARGET=armv7 
-export PIE_ENABLED=""
+export LIBVPX_TARGET=armv7
+# This is a bit hacky. C'est le bash scripts.
+export PIE_ENABLED="-fPIE -pie"
 
 ./build_ffmpeg.sh $@
